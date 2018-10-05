9b8690887ab569422cc157c2359fa9eb1e23b47e
Lewis Moore <lewis.moore2707@gmail.com>
2018-10-05 17:50:21 +0100

    Add update function ad placeholder onclick functions for updating counter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ const { div, button } = hyperscriptHelper(hyperscript)
 
 const view = (number) => {
     return div([
-        div(`Count: ${number}`),
-        button('+'),
-        button('-'),
+        div({ className: 'mv2' }, `Count: ${number}`),
+        button({ className: 'pv1 ph2 mr2', 
+        onclick: (number) => { update(number)}}, '+'),
+        button({ className: 'pv1 ph2',
+        onclick: (number) => minus(number)}, '-'),
     ])
+}
+
+const update = (buttonClicked, number) => {
+    if (buttonClicked === 'Add') {
+        return number + 1
+    } else if (buttonClicked == 'Minus') {
+        return number - 1 
+    } else {
+        return number
+    }
 }
 
 const rootNode = document.getElementById('app')
